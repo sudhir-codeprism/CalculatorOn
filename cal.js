9eb9ba284852b957114add5cb6f29ee61ac2b5a7
Sudhir Kumar <sudhirraj7836@gmail.com>
2019-12-14 09:46:35 +0530

    first commit

---
+++ cal.js
@@ -0,0 +1,19 @@
+function c(val)
+{
+document.getElementById("d").value=val;
+}
+function v(val)
+{
+document.getElementById("d").value+=val;
+}
+function e() 
+{ 
+try 
+{ 
+  c(eval(document.getElementById("d").value)) 
+} 
+catch(e) 
+{
+  c('Error') 
+} 
+}  
